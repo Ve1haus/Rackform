a8b854059eb35c601b86136c43f2d8e0de9d2278
Ve1haus <velhausstrategies@gmail.com>
2025-06-14 22:55:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 plugins: [react()],
-esbuild: {
-legalComments: 'none'
+build: {
+target: 'esnext',
+minify: 'esbuild',
+sourcemap: false
 },
 define: {
 'process.env': {}
