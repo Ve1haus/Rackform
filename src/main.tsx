ac232ce1d83479ad7eac977fc00284822bb63fb8
Ve1haus <velhausstrategies@gmail.com>
2025-06-15 08:56:00 +0100

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ConfiguratorForm from './pages/ConfiguratorForm';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ConfiguratorForm />
+  </React.StrictMode>
+);
