7b2493bf8b72ca29a5dc3db4946136b9ef506d42
Ve1haus <velhausstrategies@gmail.com>
2025-06-15 09:07:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
 root: '.',
 publicDir: 'public',
-plugins: [react()]
+plugins: [react()],
+build: {
+rollupOptions: {
+input: resolve(__dirname, 'public/index.html')
+}
+}
 });
