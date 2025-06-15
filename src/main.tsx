40e74c9306ef35122b000e533257e019dd22eb65
Ve1haus <velhausstrategies@gmail.com>
2025-06-15 08:41:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -6,11 +5,11 @@ import ConfiguratorForm from './pages/ConfiguratorForm';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ConfiguratorForm />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+<React.StrictMode>
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<ConfiguratorForm />} />
+</Routes>
+</BrowserRouter>
+</React.StrictMode>
 );
